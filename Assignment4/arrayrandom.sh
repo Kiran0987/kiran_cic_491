68fe379ad82e3ac28c038f2af73b0b1bed26b1f1
kiran <sharmakiran237@gmail.com>
2022-04-08 17:55:28 +0530

    c1

--- Assignment4/arrayrandom.sh
+++
@@ -1,9 +0,0 @@
-#generate 10 random 3 digit number
- shuf -i 100-999 -n 10  > random1.out
-cat random1.out
-# store this random number into array
-values=`cat random1.out`
- echo ${values[@]};
-
-# find 2 largest and second smallest without sorting
-printf '%s\n' "${values[@]}" | sort -n | tail -2 |head -1
