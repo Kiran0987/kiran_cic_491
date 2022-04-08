677309c8b1cb5cebe109aa136254c42d61b286a1
kiran <sharmakiran237@gmail.com>
2022-04-08 16:11:49 +0530

    c1

--- Assignment8/empwage.sh
+++ Assignment8/empwage.sh
@@ -1,10 +1,18 @@
 isPartTime=1;
 isFullTime=2
-
+MaxworkingDays=20;
 empRatePerHr=20;
-workingDays=20;
-for (( counter=1; counter<=$workingDays; counter++ ));
-do
+totalworkingHrs=0;
+MaxworkingHrs=100;
+salaryperMonth=0;
+isRunning=true;
+count=1;
+while [ $isRunning ];
+do 
+  
+         if [ $count -gt 20 ];then
+break;
+       fi
        empNo=$((RANDOM%3))
        case $empNo in
 	$isFullTime)
@@ -17,6 +25,7 @@ do
 esac
        salaryPerDay=$(($empRatePerHr * $empHrs))
        salaryPerMonth=$(($salaryPerMonth+$salaryPerDay))
+       count=$(($count+1));
 done
 echo $salaryPerDay;
 echo $salaryPerMonth;
