bf8caaa8180ab29ecee1423acc4aab6eb3391a16
kiran <sharmakiran237@gmail.com>
2022-04-09 14:30:35 +0530

    c1

--- Assignment7/coinflip.sh
+++ Assignment7/coinflip.sh
@@ -5,3 +5,17 @@ echo Head
 elif [[ ${Result} -eq 1 ]];
 then echo Tail
 fi
+$heads = 0
+$tails = 0
+$both = 0;
+      for (( $i = 0; i < 50; i+=1 ))
+      {
+        result1 =$((RANDOM%2)) 
+        result2 =$((RANDOM%2))
+        if [ $result1 && $result2 ] $heads = 1;
+        if [!($result1 && $result2) ] $tails = 1;
+        else $both=1;
+      }
+      echo("Heads: $heads");
+      echo("Tails: $tails");
+      echo("Both: $both");
